0e44d69b881379870bc4174456a0c4fa760ed5c4
william-sy <wvanbeek.sy@gmail.com>
2024-01-04 22:21:08 +0100

    minor bootstrap update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 /**
 * Template Name: Personal
-* Updated: Aug 30 2023 with Bootstrap v5.3.1
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
 * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
